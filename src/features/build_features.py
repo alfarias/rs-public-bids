973398027c7af973d61b06062aeac50f9aa282e9
alfarias <afarias@tuta.io>
2020-08-29 11:32:12 -0300

    build_features package finished

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,6 +5,8 @@ def build_features(dataset: str, data_path: str, cols: list, values: list):
     '''
     Description:
         Function to build the required fields to public bids analysis.
+        It's possible to get data on multiple folders by years name.
+        All bids are filtered by Buying status (TP_OBJETO == 'COM')
     Parameters:
         dataset: string 
             A string with the dataset name.
@@ -15,25 +17,41 @@ def build_features(dataset: str, data_path: str, cols: list, values: list):
     Return:
         A list with the built datasets.
     '''
+    print(f'Building Features for {dataset}...')
     datasets = []
-    # Iterate to get on multiple folders
+    # Iterate to get data on multiple folders
     for idx in values:
         data = pd.read_csv(data_path+os.sep+'external'+
-                            os.sep+str(idx)+os.sep+dataset,
+                            os.sep+str(idx)+os.sep+dataset+'.csv',
                             usecols=cols
                                     )
         data.drop_duplicates(inplace=True)
         # For processing "licitacao.csv"
         # Filter by Buy (COM)
-        if dataset == 'licitacao.csv':
+        if dataset == 'licitacao':
             data.query('TP_OBJETO == "COM"', inplace=True)
             data.drop(['TP_OBJETO'], axis=1, inplace=True)
             datasets.append(data)
         # For processing "item.csv"
-        elif dataset == 'item.csv':
+        elif dataset == 'item':
+            # Gets the bid year
             data['ANO_LICITACAO'] = idx
             datasets.append(data)
         else:
             pass
-
-    return datasets
+    # Merge Datasets by conditional case.
+    if len(datasets) == 1:
+        data_proc = datasets[0]
+    elif len(datasets) == 2:
+        data_proc = datasets[0].append(datasets[1])
+    else:    
+        data_proc = datasets[0].append(datasets[1])
+        for i in range(len(datasets)-2):
+            data_proc = data_proc.append(datasets[i+2])
+        data_proc.reset_index(drop=True, inplace=True)
+    # Save the features on a parquet file    
+    print('Saving extracted features to parquet file...')
+    data_proc.to_parquet(data_path+os.sep+'processed'+os.sep+
+                    str(min(values))+'_'+str(max(values))+
+                    '_'+dataset+'.parquet')
+    print('Building features process finished!')
