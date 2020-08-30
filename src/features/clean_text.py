dcc4151862b32631f3bb8501a07b4c8029d9ada1
alfarias <afarias@tuta.io>
2020-08-30 13:08:15 -0300

    General Visualizations finished.

--- src/features/clean_text.py
+++ src/features/clean_text.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
 def clean_text(df, feature: str):
+    '''
+    Description:
+        Clean text applying lower case, strip white spaces and removing
+        unwanted characters.
+    Parameters:
+        df: dataframe
+            The dataset with the text feature.
+        fearure: string
+            The Text feature/column to clean.
+    Return:
+        Cleaned text.
+    '''
     df[feature] = df[feature].str.lower()
     df[feature] = df[feature].str.strip()
     df[feature] = df[feature].str.replace(r'\d+', '')
